0d11bc5fa4fafe63657432c6e1e9e65e03876bdf
Eric Su <ejs3an@virginia.edu>
2020-10-19 10:50:15 -0400

    commented out view functions

--- sbuddy/urls.py
+++ sbuddy/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
-# from . import views
+from . import views
 
 app_name = 'sbuddy'
 urlpatterns = [
  path('', TemplateView.as_view(template_name="mysite/index.html")),
-#  path('forms/', views.forms, name='forms')
+ path('forms/', views.forms, name='forms')
 ]
